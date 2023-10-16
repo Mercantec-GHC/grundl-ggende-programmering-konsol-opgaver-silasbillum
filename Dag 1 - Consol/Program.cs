6ca198d8a0e893e84f57252793e07eb8ec0d06e8
sila1400 <sila1400@edu.mercantec.dk>
2023-10-16 13:45:53 +0200

    navn ændring

--- Dag 1 - Consol/Program.cs
+++ Dag 1 - Consol/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+//Opgave 1
+using System.Diagnostics;
+
 Console.WriteLine("Hello, World!");
+//Opgave 2
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code");
+//Opgave 3
+Console.WriteLine("");
+Console.Write("Hej med dig.");
+Console.WriteLine("");
+Console.Write("Hej tilbage.");
+Console.WriteLine("");
+
+//Store and retrieve data using literal and variable values in C#
+//Opgave 1
+Console.WriteLine("H.");
+Console.Write('S');
+Console.Write('i');
+Console.Write('l');
+Console.Write('a');
+Console.Write('s');
+Console.WriteLine("");
+//Opgave 2
+Console.WriteLine(123);
+//Opgave 3
+Console.WriteLine(0.25f);
+//Opgave 4
+Console.WriteLine(2.625);
+//Opgave 5
+Console.WriteLine(12.39816m);
+//Opgave 6
+Console.WriteLine(true);
+Console.WriteLine(false);
+//Opgave 7
+string firstName = "Silas";
+Console.WriteLine(firstName);
+//Opgave 8
+//var message = "Hello";
+//message = 10.703m;
+//Opgave 9
+string name = "bob";
+int num = 3;
+double num2 = 34.4;
+
+Console.Write("Hey " + name + " har du købt " + num + " kager til de " + num2 + " gæster");
+
+//Perform basic string formatting in C#
+
+//Exercise - Format literal strings in C#
+
+//Character escape sequences
+Console.WriteLine("Hello\nWorld");
+Console.WriteLine("Hello\nWorld");
+
+Console.WriteLine("Hello \"World\"!");
+
+
+
+
+
