5a6d82e1f5b7fd740c732acd29ba474303b21e44
sila1400 <sila1400@edu.mercantec.dk>
2023-10-17 13:03:03 +0200

    boolean

--- Dag 2 - ConsolApp/Program.cs
+++ Dag 2 - ConsolApp/Program.cs
@@ -1,3 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿/*Random dice = new Random();
+int roll = dice.Next(1, 20);
+Console.WriteLine(roll);
+*/
+//Ready for DnD.
+
+//Overloaded methods
+/*
+int number = 7;
+string text = "seven";
+Console.WriteLine(number);
+Console.WriteLine();
+Console.WriteLine(text);
+*/
+/*
+Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
+*/
+/*
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
+*/
+/*
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ( total > 14)
+{
+    Console.WriteLine("You Win!!!!!!!!");
+
+}
+if (total <=15)
+{
+    Console.WriteLine("You F##ing Loser!!!!!");
+
+}
+if((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles +2");
+    total += 2;
+}
+if ((roll1 == roll2) && (roll2 == roll3))
+{
+    Console.WriteLine("you rolled triples +6");
+}
+*/
+
+
