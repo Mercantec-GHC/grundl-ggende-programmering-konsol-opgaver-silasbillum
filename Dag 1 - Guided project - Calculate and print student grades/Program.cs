b61d82a639ec2482f993a2b8e56112afb21d8d8f
sila1400 <sila1400@edu.mercantec.dk>
2023-10-17 11:12:33 +0200

    part 1

--- Dag 1 - Guided project - Calculate and print student grades/Program.cs
+++ Dag 1 - Guided project - Calculate and print student grades/Program.cs
@@ -25,7 +25,7 @@ int jeong3 = 98;
 int jeong4 = 100;
 int jeong5 = 97;
 
-Console.WriteLine("Student   Grade");
+/*Console.WriteLine("Student   Grade");
 decimal sop = sophia1+ sophia2+ sophia3+ sophia4+ sophia5;
 Console.WriteLine("Sophia    " + sop /5 + " A"  );
 decimal nic = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
@@ -34,3 +34,48 @@ decimal zah = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
 Console.WriteLine("Zahirah   "+ zah /5 + " B ");
 decimal jeo = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
 Console.WriteLine("Jeong     " + jeo/5 + " A ");
+*/
+//Exercise - Calculate the sum of each student's assignment scores
+
+//Create variables to store the sum
+
+
+
+
+int sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+int nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
+int zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
+int jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
+
+/*Console.WriteLine("Sophia: " + sophiaSum);
+Console.WriteLine("Nicolas: " + nicolasSum);
+Console.WriteLine("Zahirah: " + zahirahSum);
+Console.WriteLine("Jeong: " + jeongSum);
+*/
+//Exercise - Calculate the average of the student's assignment scores
+
+//Create variables to store the average
+
+decimal sophiaScore = (decimal) sophiaSum / currentAssignments;
+decimal nicolasScore = (decimal) nicolasSum / currentAssignments;
+decimal zahirahScore = (decimal) zahirahSum / currentAssignments;
+decimal jeongScore = (decimal) jeongSum / currentAssignments;
+
+/*Console.WriteLine("Sophia: " + sophiaScore + " A");
+Console.WriteLine("Nicolas: " + nicolasScore + " B");
+Console.WriteLine("Zahirah: " + zahirahScore + " B");
+Console.WriteLine("Jeong: " + jeongScore + " A");
+*/
+//Exercise - Format the output using escaped character sequences
+
+//Format the console output
+/*
+Console.WriteLine("Student Grade\n");
+Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA");
+Console.WriteLine("Nicolas:\t" + nicolasScore + "\tB");
+Console.WriteLine("Zahirah:\t" + zahirahScore + "\tB");
+Console.WriteLine("Jeong:\t\t" + jeongScore + "\tA");
+*/
+
+//Prepare for guided project
+
--- Dag 1 - Guided project - Calculate final GPA/Program.cs
+++ Dag 1 - Guided project - Calculate final GPA/Program.cs
@@ -10,3 +10,92 @@ int course2Credit = 3;
 int course3Credit = 4;
 int course4Credit = 4;
 int course5Credit = 3;
+/*
+Console.WriteLine("Student:" + studentName);
+Console.WriteLine("Course       " + "    Grade  " + "Credit Hours");
+Console.WriteLine(course1Name + "         4      " +     course1Credit);
+Console.WriteLine(course2Name + "         3      " +     course2Credit);
+Console.WriteLine(course3Name + "         3      " +     course3Credit);
+Console.WriteLine(course4Name + "  3      " +     course4Credit);
+Console.WriteLine(course5Name + "      4      " +     course5Credit);
+
+decimal GPA = course1Credit + course2Credit + course3Credit + course4Credit + course5Credit;
+Console.WriteLine("\nFinal GPA:           " + GPA /5);
+*/
+//Exercise - Store the numeric grade values for each course
+
+//Create variables to store the grade values
+
+int gradeA = 4;
+int gradeB = 3;
+
+int course1Grade = gradeA;
+int course2Grade = gradeB;
+int course3Grade = gradeB;
+int course4Grade = gradeB;
+int course5Grade = gradeA;
+/*
+Console.WriteLine($"\n{course1Name} {course1Grade} {course1Credit}");
+Console.WriteLine($"{course2Name} {course2Grade} {course2Credit}");
+Console.WriteLine($"{course3Name} {course3Grade} {course3Credit}");
+Console.WriteLine($"{course4Name} {course4Grade} {course4Credit}");
+Console.WriteLine($"{course5Name} {course5Grade} {course5Credit}");
+*/
+//Exercise - Calculate the sums of credit hours and grade points
+
+//Create variables to store the average
+
+int totalCreditHours = 0;
+
+totalCreditHours += course1Credit;
+totalCreditHours += course2Credit;
+totalCreditHours += course3Credit;
+totalCreditHours += course4Credit;
+totalCreditHours += course5Credit;
+
+totalCreditHours = course1Credit + course2Credit + course3Credit + course4Credit + course5Credit;
+
+int totalGradePoints = 0;
+
+totalGradePoints += course1Credit * course1Grade;
+
+totalGradePoints += course2Credit * course2Grade;
+totalGradePoints += course3Credit * course3Grade;
+totalGradePoints += course4Credit * course4Grade;
+totalGradePoints += course5Credit * course5Grade;
+
+
+Console.WriteLine($"{course1Name} {course1Grade} {course1Credit}");
+Console.WriteLine($"{course2Name} {course2Grade} {course2Credit}");
+Console.WriteLine($"{course3Name} {course3Grade} {course3Credit}");
+Console.WriteLine($"{course4Name} {course4Grade} {course4Credit}");
+Console.WriteLine($"{course5Name} {course5Grade} {course5Credit}");
+
+//Exercise - Format the decimal output
+
+//Calculate the final GPA
+/*
+decimal gradePointAverage = (decimal) totalGradePoints/totalCreditHours;
+
+Console.WriteLine($"Final GPA: {gradePointAverage}");
+
+//Format the decimal output
+
+int leadingDigit = (int) gradePointAverage;
+int firstDigit = (int) (gradePointAverage * 10) % 10;
+int secondDigit = (int) (gradePointAverage * 100) % 10;
+Console.WriteLine($"Final GPA: {leadingDigit}.{firstDigit}{secondDigit}");
+
+//Exercise - Format the output using escaped character sequences
+
+//Format the console output
+
+Console.WriteLine($"Student: {studentName}\n");
+Console.WriteLine("Course\t\t\t\tGrade\tCredit Hours");
+Console.WriteLine($"{course1Name}\t\t\t{course1Grade}\t\t{course1Credit}");
+Console.WriteLine($"{course2Name}\t\t\t{course2Grade}\t\t{course2Credit}");
+Console.WriteLine($"{course3Name}\t\t\t{course3Grade}\t\t{course3Credit}");
+Console.WriteLine($"{course4Name}\t{course4Grade}\t\t{course4Credit}");
+Console.WriteLine($"{course5Name}\t\t{course5Grade}\t\t{course5Credit}");
+*/
+
