c0a935bc62bf2f7316b5c262f85cb3c0e8383272
sila1400 <sila1400@edu.mercantec.dk>
2023-10-18 10:22:21 +0200

    dag 2

--- Dag 2 - ConsolApp/Program.cs
+++ Dag 2 - ConsolApp/Program.cs
@@ -117,5 +117,216 @@ Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
 
 Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process");
 */
+/*
+using System.Diagnostics.CodeAnalysis;
+
+int[] inventory = {200, 450, 700, 175, 250};
+int sum = 0;
+int bin = 0;
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin{bin} = {items} items (running total: {sum})");
+}
+Console.WriteLine($"We Have {sum} items in inventory");
+*/
+/*
+using System.Globalization;
+
+string[] num = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+foreach (string order in num)
+{
+    if(order.StartsWith("A"))
+    {
+        Console.WriteLine(order);
+    }
+}
+*/
+/*
+string firstName = "Bob";
+int widgetsPurchased = 7;
+// Testing a change to the message.
+// int widgetsSold = 7;
+// Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
+Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
+*/
+
+/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+/*
+Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
+{
+   
+    int prefixValue = random.Next(65, 70);
+   
+    string prefix = Convert.ToChar(prefixValue).ToString();
+   
+    string suffix = random.Next(1, 1000).ToString("000");
+   
+    orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
+*/
+/*
+string str = "The quick brown fox jumps over the lazy dog.";
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+// Reverse the chars
+Array.Reverse(charMessage);
+int x = 0;
+// count the o's
+foreach (char i in charMessage) { if (i == 'o') { x++; } }
+// convert it back to a string
+string new_message = new String(charMessage);
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
+*/
+/*
+int currentAssignments = 5;
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+int sophiaSum = 0;
 
 
+decimal sophiaScore;
+foreach (int score in sophiaScores)
+{
+    sophiaSum += score;
+}
+
+
+
+sophiaScore = (decimal)sophiaSum / currentAssignments;
+
+Console.WriteLine("Student\t\tGrade\n");
+Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+*/
+// initialize variables - graded assignments 
+/*
+int currentAssignments = 5;
+int examAssignments = 5;
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
+int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };    
+
+
+// Student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
+int[] studentScores = new int[10];
+string currentStudentLetterGrade = "";
+
+Console.WriteLine("Student\tExam Score\tOverall Grade\tExtra Credit\n");
+
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+        
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    else if (currentStudent == "Becky")
+    studentScores = beckyScores;
+ else if (currentStudent == "Chris")
+    studentScores = chrisScores;
+ else if (currentStudent == "Eric")
+    studentScores = ericScores;
+ else if (currentStudent == "Gregor")
+    studentScores = gregorScores;
+ else
+    continue;
+
+    
+    int sumAssignmentScores = 0;
+
+   
+    decimal currentStudentGrade = 0;
+
+    int gradedAssignments = 0;
+
+    foreach (int score in studentScores)
+    {
+        gradedAssignments += 1;
+        if (gradedAssignments <= examAssignments)
+        sumAssignmentScores += score;
+        else sumAssignmentScores += score /10;
+    }
+
+    currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignments;
+
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+    
+    else currentStudentLetterGrade = "F";
+
+    
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t");
+
+}
+  */  
--- Dag 3 - ConsolApp/Program.cs
+++ Dag 3 - ConsolApp/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
+// These two lines of code will create the same output
+
+Console.WriteLine(pangram.Contains("fox") == false);
+Console.WriteLine(!pangram.Contains("fox"));
+*/
+/*string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(!pangram.Contains("fox"));
+Console.WriteLine(!pangram.Contains("cow"));
+*/
+/*
+int saleAmount = 1001;
+
+Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+*/
+Random flip = new Random();
+int num = flip.Next(2);
+if (num == 0)
+{
+    Console.WriteLine("Heads");
+}
+else if (num == 1)
+{
+    Console.WriteLine("Tails");
+}
+Console.WriteLine(num);
